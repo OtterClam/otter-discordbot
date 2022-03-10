8cb2ed1ca35c40db5c32f7e81545bbd162eec933
d0c0q <d0c0q@otterclam.finance>
2022-03-10 10:18:01 +0800

    feat: sheet store for ottolisted bot

--- src/ottolistedBot.js
+++ src/ottolistedBot.js
@@ -1,10 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { newClient, registerSlashCommand } = require('./discord')
+const { newSheetsClient } = require('./google')
 
-const wait = require('node:timers/promises').setTimeout
-
-const ottolistedBot = async ({ token, clientId, guildId }) => {
+const ottolistedBot = async ({
+  token,
+  clientId,
+  guildId,
+  sheetEmail,
+  sheetPriKey,
+  sheetId,
+}) => {
   const client = newClient()
+  const sheet = await newSheetsClient({
+    email: sheetEmail,
+    priKey: sheetPriKey,
+    sheetId: sheetId,
+  })
 
   const commands = [
     new SlashCommandBuilder()
@@ -28,23 +39,41 @@ const ottolistedBot = async ({ token, clientId, guildId }) => {
     if (!interaction.isCommand()) return
     if (interaction.commandName !== 'ottolisted') return
 
-    await interaction.deferReply({ ephemeral: true })
     const reply = m => interaction.editReply({ content: m, ephemeral: true })
+    try {
+      await interaction.deferReply({ ephemeral: true })
 
-    const wallet = interaction.options.getString('wallet')
-    if (!wallet.match(/^0x[0-9a-fA-F]{40}$/)) {
-      await reply(`${wallet} is not a valid wallet address.`)
-      return
+      const wallet = interaction.options.getString('wallet')
+      if (!wallet.match(/^0x[0-9a-fA-F]{40}$/)) {
+        await reply('Invalid wallet address.')
+        return
+      }
+      const rolesSheet = sheet.sheetsByTitle['Roles']
+
+      const roles = (await rolesSheet.getRows())?.map(r => r && r.Name) ?? []
+      if (!interaction.member.roles.cache.some(r => roles.includes(r.name))) {
+        await reply('You are not ottolisted!')
+        return
+      }
+
+      const addressesSheet = sheet.sheetsByTitle['Addresses']
+      const ids = (await addressesSheet.getRows())?.map(r => r && r.ID) ?? []
+      if (ids.some(id => id === interaction.user.id)) {
+        await reply(`Already submitted.`)
+        return
+      }
+      await addressesSheet.addRow({
+        ID: interaction.user.id,
+        Name: interaction.user.username,
+        Wallet: wallet,
+      })
+
+      // update google google sheet
+      await reply('Submitted!')
+    } catch (err) {
+      console.error(err)
+      reply(`Unexpected error`).catch(console.error)
     }
-
-    if (!interaction.member.roles.cache.some(r => r.name === 'ottolisted')) {
-      await reply('You are not in ottolisted!')
-      return
-    }
-
-    // update google google sheet
-    await wait(1000)
-    await reply('Pong!')
   })
 
   return client.login(token)
